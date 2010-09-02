988d4a85aca3e8b018d188b4684e49d0f86961d0
paul.ishenin <paul.ishenin@c8cb53cb-5a30-489f-1e97-fdf6cf4627ce>
2010-09-02 18:22:59 +0000

    cleanup

--- trunk/src/com/paulish/widgets/stocks/TickersEditActivity.java
+++ trunk/src/com/paulish/widgets/stocks/TickersEditActivity.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import mobi.intuitit.android.content.LauncherIntent;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.appwidget.AppWidgetManager;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -23,7 +20,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Button;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class TickersEditActivity extends Activity implements OnClickListener, OnItemClickListener {
