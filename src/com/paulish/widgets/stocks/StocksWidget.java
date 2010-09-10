13fee6c2778b11abc3f3196eec44a2cd445f2d06
paul.ishenin <paul.ishenin@c8cb53cb-5a30-489f-1e97-fdf6cf4627ce>
2010-09-10 17:28:50 +0000

    - remove null state image, use zero instead - add arrow images for single quote widget - broadcast loading state change to all widgets and filter loading state properly - continue work on single quote widget

--- src/com/paulish/widgets/stocks/StocksWidget.java
+++ src/com/paulish/widgets/stocks/StocksWidget.java
@@ -2,6 +2,8 @@ package com.paulish.widgets.stocks;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
@@ -9,7 +11,7 @@ public abstract class StocksWidget extends AppWidgetProvider {
 	// Tag for logging
 	protected static final String TAG = "paulish.StocksWidget";
 	// Actions
-	public static final String ACTION_WIDGET_NOTIFY_LOADING = "notify_loading";
+	public static final String ACTION_NOTIFY_LOADING = "com.paulish.widgets.stocks.action.NOTIFY_LOADING";
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -28,23 +30,16 @@ public abstract class StocksWidget extends AppWidgetProvider {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		final String action = intent.getAction();
-		// Log.d(TAG, "received -> " +  action);
-		if  (ACTION_WIDGET_NOTIFY_LOADING.equals(action)) {
-			final int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
-					AppWidgetManager.INVALID_APPWIDGET_ID);
-			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+		//Log.d(TAG, "received -> " +  action);
+		if  (ACTION_NOTIFY_LOADING.equals(action)) {
+			final int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,	AppWidgetManager.INVALID_APPWIDGET_ID);
+			if ((appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) && appWidgetClassMatch(context, appWidgetId)) {
 				updateWidget(context, appWidgetId, intent.getExtras().getBoolean("loading"));
 			}			
-		} else if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
-			final int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
-					AppWidgetManager.INVALID_APPWIDGET_ID);
-			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
-				this.onDeleted(context, new int[] { appWidgetId });
-			}
 		} else
 			super.onReceive(context, intent);
 	}
-
+	
 	/**
 	 * Will be executed when the widget is removed from the home screen 
 	 */
@@ -59,10 +54,18 @@ public abstract class StocksWidget extends AppWidgetProvider {
 		// Drop the settings if the widget is deleted
 		Preferences.DropSettings(context, appWidgetIds);
 	}	
-				
+					
+	protected boolean appWidgetClassMatch(Context context, int appWidgetId) {
+        final AppWidgetManager awm = AppWidgetManager.getInstance(context);
+        final AppWidgetProviderInfo info = awm.getAppWidgetInfo(appWidgetId);
+        if (info != null)
+        	return info.provider.equals(new ComponentName(context, getClass()));
+        else
+        	return false;
+	}
+	
 	public static void setLoading(Context context, Integer[] appWidgetIds, boolean loading) {
-		final Intent intent = new Intent(context, StocksWidget.class);
-		intent.setAction(ACTION_WIDGET_NOTIFY_LOADING);
+		final Intent intent = new Intent(ACTION_NOTIFY_LOADING);
 		intent.putExtra("loading", loading);
 		for (int appWidgetId : appWidgetIds) {
 			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
--- src/com/paulish/widgets/stocks/receivers/StocksWidgetSingle.java
+++ src/com/paulish/widgets/stocks/receivers/StocksWidgetSingle.java
@@ -1,19 +1,29 @@
 package com.paulish.widgets.stocks.receivers;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.View.OnTouchListener;
 import android.widget.RemoteViews;
 
+import com.paulish.widgets.stocks.QuoteViewActivity;
 import com.paulish.widgets.stocks.R;
+import com.paulish.widgets.stocks.StocksProvider;
 import com.paulish.widgets.stocks.StocksWidget;
 
-public class StocksWidgetSingle extends StocksWidget {
+public class StocksWidgetSingle extends StocksWidget /*implements OnTouchListener*/ {
+	
+	private int currentIndex = 0;
 	
 	@Override
 	protected void updateWidget(Context context, int appWidgetId, Boolean loading) {
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stocks_widget_single);
+        
         updateWidgetData(context, appWidgetId, loading, views);
-        final AppWidgetManager awm = AppWidgetManager.getInstance(context);
+        final AppWidgetManager awm = AppWidgetManager.getInstance(context);        
         awm.updateAppWidget(appWidgetId, views);		
 	}
 	
@@ -22,19 +32,52 @@ public class StocksWidgetSingle extends StocksWidget {
 			return;
 		
 		// query the data
-		views.setTextViewText(R.id.quoteSymbol, "GOOG");
-		views.setTextViewText(R.id.quotePrice, "1024");
-		views.setTextViewText(R.id.quoteChangePercent, "+10.20%");
-		views.setTextViewText(R.id.quoteChange, "+40.25");
-		/* TODO: missing drawables 
-		if (quoteChange == null)
-			views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_state_unknown);
-		else if (quoteChange == 0)
-			views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_state_zero);
-		else if (quoteChange < 0)
-			views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_state_negative);
-		else
-			views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_state_positive); */		
+		Uri quotes = StocksProvider.CONTENT_URI_WIDGET_QUOTES.buildUpon().appendEncodedPath(
+				Integer.toString(appWidgetId)).build();
+		
+		Cursor cur = context.getContentResolver().query(quotes, StocksProvider.PROJECTION_QUOTES, null, null, null);
+		
+		if (cur != null) {
+			final int lastIndex = cur.getCount() - 1;
+			if (currentIndex > lastIndex)
+				currentIndex = 0;
+			if (currentIndex == -1)
+				currentIndex = lastIndex;
+
+			if (!cur.moveToPosition(currentIndex))
+				return;
+						
+			final String symbol = cur.getString(StocksProvider.QuotesColumns.symbol.ordinal());
+			
+			views.setTextViewText(R.id.quoteSymbol, symbol);
+			views.setTextViewText(R.id.quotePrice, cur.getString(StocksProvider.QuotesColumns.price.ordinal()));
+			views.setTextViewText(R.id.quoteChangePercent, cur.getString(StocksProvider.QuotesColumns.pchange.ordinal()));
+			views.setTextViewText(R.id.quoteChange, cur.getString(StocksProvider.QuotesColumns.change.ordinal()));
+			switch (cur.getInt(StocksProvider.QuotesColumns.stateimage.ordinal())) {
+			case R.drawable.stocks_widget_state_red:
+				views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_arrow_negative);
+				break;
+			case R.drawable.stocks_widget_state_green:
+				views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_arrow_positive);
+				break;
+			default:
+				views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_arrow_zero);
+				break;
+			}
+			cur.close();
+
+			Intent openForSymbolIntent = QuoteViewActivity.getOpenForSymbolIntent(context, symbol);
+	        views.setOnClickPendingIntent(R.id.widgetLayout, 
+	        		PendingIntent.getActivity(context, appWidgetId, openForSymbolIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+			
+		} else {
+			// fill with default values 
+			views.setTextViewText(R.id.quoteSymbol, "");
+			views.setTextViewText(R.id.quotePrice, "0");
+			views.setTextViewText(R.id.quoteChangePercent, "0.0%");
+			views.setTextViewText(R.id.quoteChange, "0.0");
+			views.setImageViewResource(R.id.stateImage, R.drawable.stocks_widget_arrow_zero);			
+		}			
 	}
 
 }
